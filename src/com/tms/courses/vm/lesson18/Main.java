64445878024b52e39cb70b06f781b4bb4031a693
¨Viachaslau_Mukhin¨ <viachaslau.mukhin@expert-soft.by>
2022-07-28 20:47:27 +0300

    Add example of 18 lesson

---
+++ src/com/tms/courses/vm/lesson18/Main.java
@@ -0,0 +1,59 @@
+package com.tms.courses.vm.lesson18;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException
+    {
+        File file = new File("/home/user/example_git_project/src/com/tms/courses/vm/lesson18/test.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
+        doc.getDocumentElement().normalize();
+        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+        NodeList nList = doc.getElementsByTagName("student");
+        System.out.println("----------------------------");
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                System.out.println("Student roll no : "
+                        + eElement.getAttribute("rollno"));
+                System.out.println("First Name : "
+                        + eElement
+                        .getElementsByTagName("firstname")
+                        .item(0)
+                        .getTextContent());
+                System.out.println("Last Name : "
+                        + eElement
+                        .getElementsByTagName("lastname")
+                        .item(0)
+                        .getTextContent());
+                System.out.println("Nick Name : "
+                        + eElement
+                        .getElementsByTagName("nickname")
+                        .item(0)
+                        .getTextContent());
+                System.out.println("Marks : "
+                        + eElement
+                        .getElementsByTagName("marks")
+                        .item(0)
+                        .getTextContent());
+            }
+        }
+    }
+}
